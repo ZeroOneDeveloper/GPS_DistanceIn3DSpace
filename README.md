7b83266bf6f6df7430333001a74c2a8bb38833b5
ZeroOneDeveloper <me@zero0ne.dev>
2023-11-26 15:49:46 +0900

    feat: calculating func

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # GPS_DistanceIn3DSpace
 ## Geometric Exploration Tasks for the 2nd Semester of 2023
 
-
--- main.py
+++ main.py
@@ -1,1 +1,27 @@
 import gradio as gr
+
+import math
+
+with gr.Blocks() as demo:
+    with gr.Row():
+        with gr.Column():
+            latitudes1 = gr.Number(label="Latitude (위도)", info='-180 ~ 180')
+            longitudes1 = gr.Number(label="Longitude (경도)", info='-90 ~ 90')
+        with gr.Column():
+            latitudes2 = gr.Number(label="Latitude (위도)", info='-180 ~ 180')
+            longitudes2 = gr.Number(label="Longitude (경도)", info='-90 ~ 90')
+    RunButton = gr.Button("Run")
+    Distance = gr.Textbox(value="", label="Distance (m)")
+
+    def EquirectangularApproximation(x1, y1, x2, y2):
+        x = (x2 - x1) * math.cos(0.5 * (y2 + y1))
+        y = y2 - y1
+        d = math.sqrt(x ** 2 + y ** 2) * 6371e3
+        return d
+
+    RunButton.click(EquirectangularApproximation, inputs=[latitudes1, longitudes1, latitudes2, longitudes2], outputs=[Distance])
+
+demo.launch(
+    server_name='0.0.0.0',
+    server_port=7878,
+)
